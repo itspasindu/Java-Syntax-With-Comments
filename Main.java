181e3ea85107481cadc9374eec37175dabe09b12
Pasindu Dewviman <pasindudewviman59@gmail.com>
2024-12-23 10:06:48 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); // Display the string. print(ln) is a use for get the new line. other method is using \n for new line in the string. ("Hello, \nWorld!")
+
+        System.out.println("Welcome to Java Programming!");
+
+        System.out.println("\"show double quotes in a string\""); // Display the string with double quotes.
+
+        /*
+         * Multi-line comment
+         */
+
+        
+    }
+}
---
+++ Varibales.java
@@ -0,0 +1,37 @@
+public class Varibales {
+    public static void main(String[] args) {
+        // x = 10; initialize the variable x with a value of 10
+        int x; // Declare a variable x
+        x = 10; // Assign a value to x
+
+        double y = 3.14; // Declare and initialize a variable y with a value of 3.14
+        boolean z = true; // Declare and initialize a variable z with a value of true
+
+        System.out.println(x); // Display the value of x. don't use "" for the variable.
+
+        System.out.println("My Number Is: "+ x + "!"); // Display the value of x with a string. use "" for the string.
+        System.out.println("Precentage Is: " + y);
+        System.out.println("Boolean Value: " + z);
+    }
+}
+
+/*
+ * boolean - true or false
+ * 
+ * byte - -128 to 127 (inclusive)
+ * 
+ * short - -32,768 to 32,767 (inclusive)
+ * 
+ * int - -2billion to 2billion (inclusive)
+ * 
+ * long - -9quintillion to 9quintillion (inclusive)
+ * 
+ * float - fraction number up to 6 to 7 decimal digits. ex: 3.14f
+ * 
+ * double - fraction number up to 15 decimal digits. ex: 3.14
+ * 
+ * char - single character/letter or ASCII value. ex: 'a', 'b', 'c'
+ * 
+ * String - a sequence of characters or a text. ex: "Hello, World!"
+ * 
+ */
