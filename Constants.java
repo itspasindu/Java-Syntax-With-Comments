c2b888582eb10981d2cf583733541cfc8c9764a2
Pasindu Dewviman <pasindudewviman59@gmail.com>
2024-12-24 09:23:24 +0530

    added Constants

---
+++ Constants.java
@@ -0,0 +1,8 @@
+public class Constants {
+    public static void main(String[] args) {
+        final double PI = 3.14159; // Declare a constant variable PI
+        // PI = 3.14; // Compile-time error: cannot assign a value to a final variable
+
+        System.out.println(PI); // Output: 3.14159
+    }
+}
